0369914dc5a0a993b969ab35154c0c7d8d839e55
drewablo <andrewt77@gmail.com>
2014-05-09 08:31:18 -0500

    Update ohgod.js

--- ohgod.js
+++ ohgod.js
@@ -8,13 +8,16 @@ function liner(){
 	r_text[5] = "I can't remember the last time I got 8 hours of sleep";
 	r_text[6] = "Who's got time to do laundry?";
 	r_text[7] = "Did you see what was on CapFax this morning?";
-    r_text[8] = "I need a drink";
-    r_text[9] = "Not another resolution!";
-    r_text[10]= "Sine Die ... Psych!";
-    r_text[11] = "I've forgotten what my family's faces look like";
+    	r_text[8] = "I need a drink";
+	r_text[9] = "Not another resolution!";
+	r_text[10]= "Sine Die ... Psych!";
+	r_text[11] = "I've forgotten what my family's faces look like";
 
 	var i = Math.floor(12*Math.random());
 
 	document.getElementById('headline').innerHTML = r_text[i];
 }
 	
+function fader(){
+	document.getElementById('headline')
+}
